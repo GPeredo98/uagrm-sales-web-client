ef935ef2a1d0ef6ea82b275e12ededccd2c770e4
Gabriel Peredo <gperedo@overscope.tech>
2024-06-27 16:44:58 -0400

    running tests

--- karma.conf.js
+++ karma.conf.js
@@ -1,16 +1,30 @@
-module.exports = function(config) {
+process.env.CHROME_BIN = require('puppeteer').executablePath();
+
+module.exports = function (config) {
   config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
+    client: {
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
+    },
+    coverageIstanbulReporter: {
+      dir: require('path').join(__dirname, './coverage/your-project-name'),
+      reports: ['html', 'lcovonly', 'text-summary'],
+      fixWebpackSourcePaths: true
+    },
+    reporters: ['progress', 'kjhtml'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
     browsers: ['ChromeHeadless'],
-    customLaunchers: {
-      ChromeHeadless: {
-        base: 'Chrome',
-        flags: [
-          '--headless',
-          '--no-sandbox',
-          '--disable-gpu',
-          '--disable-dev-shm-usage'
-        ]
-      }
-    }
+    singleRun: true,
+    restartOnFileChange: true
   });
 };
